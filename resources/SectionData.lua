97335d58862f8ccac70586b9f03b78a184432bf2
unope <unope@LAPTOP-4IMDQARE>
2022-07-14 21:40:38 -0400

    Create section data file

---
+++ resources/SectionData.lua
@@ -0,0 +1,121 @@
+--[[
+Pending routing questions:
+-are we taking Lileep or Cradily in PB4?
+--]]
+
+sectionPokes = {
+	["Pre-Badge 1"]={
+		258, 259, 260,--Mudkip line
+		261, 262, --Poochyena line
+		263, 264, --Zigzagoon line
+		265, 266, 267, 268, 269, --Wurmple lines
+		270, 271, --Lotad line
+		276, 277, --Taillow line
+		278, 279, --Wingull line
+		280, 281, 282, --Ralts line
+		283, 284, --Surskit line
+		285, 286, --Shroomish line
+		287, 288, 289, --Slakoth line
+		290, 291, 292, --Nincada line
+		293, 294, 295, --Whismur line
+		296, 297, --Makuhita line
+		300 --Skitty
+	},
+	["Pre-Badge 2"]={
+		41, 42, 169, --Zubat line
+		43, 44, --Oddish line
+		63, 64, --Abra line
+		72, 73, --Tentacool line
+		74, 75, --Geodude line
+		118, 119, --Goldeen line
+		129, 130, --Magikarp line
+		183, 184, --Marill line
+		302, --Sableye
+		304, 305, 306, --Aron line
+		309, 310, --Electrike line
+		311, 312, --Plusle & Minun
+		313, 314, --Volbeat & Illumise
+		315, --Roselia
+		316, 317 --Gulpin line
+	},
+	["Pre-Badge 3"]={
+		27, 28, --Sandshrew line
+		66, 67, --Machop line
+		88, 89, --Grimer line
+		109, 110, --Koffing line
+		360, 202 --Wobbuffet line
+		218, 219, --Slugma line
+		227, --Skarmory
+		299, --Nosepass
+		301, --Delcatty
+		322, 323, --Numel line
+		324, --Torkoal
+		325, 326, --Spoink line
+		327, --Spinda
+		333, 334, --Swablu line
+		336, --Seviper
+		337, --Lunatone
+	},
+	["Pre-Badge 4"]={
+		328, 329, 330, --Trapinch line
+		331, 332, --Cacnea line
+		343, 344, --Baltoy line
+		345, 346 --Lileep line 347, 348 --Anorith line
+	},
+	["Pre-Badge 6"]={
+		172, 25, 26, --Pikachu line
+		37, 38, --Vulpix line
+		174, 39, 40, --Jigglypuff line
+		45, 182, --Vileplume & Bellossom
+		54, 55, --Psyduck line
+		81, 82, --Magnemite line
+		84, 85, --Doduo line
+		100, 101, --Voltorb line
+		111, 112, --Rhyhorn line
+		116, 117, --Horsea line
+		120, --Staryu
+		127, --Pinsir
+		177, 178, --Natu line
+		203, --Girafarig
+		214, --Heracross
+		222, --Corsola line
+		231, 232, --Phanpy line
+		298, --Azurill
+		307, 308, --Meditite line
+		318, 319, --Carvanha line
+		320, 321, --Wailmer line
+		339, 340, --Barboach line
+		341, 342, --Corphish line
+		349, 350, --Feebas line
+		351, --Castform
+		352, --Kecleon
+		353, 354, --Shuppet line
+		355, 356, --Duskull line
+		357, --Tropius
+		358, --Chimecho
+		359, --Absol
+		361, 362, --Snorunt line
+		363, 364, 365, --Spheal line
+		370 --Luvdisc
+	},
+	["Pre-Badge 7"]={
+		121, --Starmie
+		170, 171, --Chinchou line
+		272, --Ludicolo
+		366, --Clamperl
+		369, --Relicanth
+		377, 378, --Regirock & Regice
+		382 --Kyogre
+	},
+	["Pre-Badge 8"]={
+		371, 372, 373 --Bagon line
+	},
+	["Pre-Elite 4"]={
+		379 --Registeel
+	},
+	["Postgame"]={
+		374, 375, 376, --Beldum line
+		380, --Latias
+		384 --Rayquaza
+	},
+}
